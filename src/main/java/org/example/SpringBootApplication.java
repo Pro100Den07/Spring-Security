f39df42564b7fa1a67f68f4ba8da2241fa937c84
Pro100Den07 <157143837+Pro100Den07@users.noreply.github.com>
2025-02-25 18:39:49 +0200

    Update SpringBootApplication.java

--- src/main/java/org/example/SpringBootApplication.java
+++ src/main/java/org/example/SpringBootApplication.java
@@ -1,4 +1,15 @@
 package org.example;
 
-public @interface SpringBootApplication {
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
 }
